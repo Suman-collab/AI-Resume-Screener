9c18b24652fc5adb5116bd9893998c5e4efa973c
Suman <s61354922@gmail.com>
2026-03-20 12:15:27 +0530

    add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target:"http://localhost:5000/",
+          target:"https://ai-resume-screener-nw47.vercel.app/",
           changeOrigin: true,
         },
       },
